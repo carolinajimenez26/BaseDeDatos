62405f3b6cbe85197c5e088a74cba38e8eccfa74
Carolina <krito_j_g@utp.edu.co>
2016-05-12 15:44:03 -0500

    Tratando de graficar de forma general cualquier json en una tabla html

--- web/browser/main.js
+++ web/browser/main.js
@@ -6,18 +6,46 @@ io.on('showData',function(data, path){
   var content = document.createTextNode(rows[0].nombre);
   UData.appendChild(content);*/
 
-  var cols = Object.keys(data).length;
-  var rows = Object.keys(data[0]).length;//todas las filas tienen la misma info, por ende el mismo tamaño
+  var rows = Object.keys(data).length;
+  var cols = Object.keys(data[0]).length;//todas las columnas tienen la misma info, por ende el mismo tamaño
 
   console.log("col : " + cols);
   console.log("rows : " + rows);
+
+  var list = document.getElementById("tableU");
+
+  /*for(var i = 0; i < rows; i++){
+    var tr = document.createElement('tr');
+    //for(var j = 0; j < cols; j++){
+      var td1 = tr.appendChild(document.createElement('td'));
+      td1.innerHTML = data[i].MATRICULA;//i;//data[rows][cols];
+      list.appendChild(tr);
+      var td2 = tr.appendChild(document.createElement('td'));
+      td2.innerHTML = data[i].NOMBRES;
+      list.appendChild(tr);
+      var td3 = tr.appendChild(document.createElement('td'));
+      td3.innerHTML = data[i].APELLIDOS;
+      list.appendChild(tr);
+    //}
+  }*/
+
+  var columnas = ['MATRICULA','NOMBRES','APELLIDOS'];
+
+  //console.log(" : " , Object.keys(data[0])[0]);
+  //console.log(" : " , Object.keys(data[0])[1]);
+
+  var aux = Object.keys(data[0])[0];
+  console.log(Object.keys(data[0]));//devuelve las llaves de ese objeto
 
   for(var i = 0; i < rows; i++){
     var tr = document.createElement('tr');
     for(var j = 0; j < cols; j++){
       var td = tr.appendChild(document.createElement('td'));
-      td.innerHTML = data[rows][cols];
-      document.getElementById("tableU").appendChild(tr);
+      var aux = Object.keys(data[0])[j];
+      console.log(aux);
+
+      td.innerHTML = data[i].aux;//i;//data[rows][cols];
+      list.appendChild(tr)
     }
   }
 
--- web/routes/index.js
+++ web/routes/index.js
@@ -5,50 +5,11 @@ var bodyParser = require('body-parser');//para sacar los datos de los formulario
 var mysql = require('mysql');//bases de datos
 
 
-//------------------Base de datos---------------
-
-/*var connection = {
-  connected: false,
-  conn: {}
-}
-
-function StartBD(){
-
-  var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'drogueria'
-  });
-
-  connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      var ans = {
-        connected: false,
-        conn: connection
-      };
-    }else{
-      console.log('connected as id ' + connection.threadId);
-      //consult(connection);
-      connected = true;
-      //return connection;
-      var ans = {
-        connected: true,
-        conn: connection
-      };
-    }
-    //console.log("ans : ", ans);
-    return ans;
-  });
-
-}*/
-
 function consult(connection, socket, callback){
   if(connection === undefined){
     console.error('No se ha definido la conexión ');
   }else{
-    var query = connection.query('SELECT * FROM universidades',
+    var query = connection.query('SELECT * FROM alumnos',
       function(error, rows){
         if(error){
           throw error;
@@ -66,13 +27,8 @@ function emitter(socket, ans){
   socket.emit('showData', ans);//envia los datos
 }
 
-function handleU(socket, info, connection){
-  console.log("handleU");
-  consult(connection, socket, emitter);
-}
-
-function handlePedidos(socket, info, connection){
-  var rows = consult(connection);
+function handleAlumnos(socket, info, connection){
+  var rows = consult(connection, socket, emitter);
 }
 
 //---------------Exportaciones-------------------------
@@ -87,7 +43,7 @@ module.exports = function(app, mountPoint){
     host     : 'localhost',
     user     : 'root',
     password : '',
-    database : 'drogueria'
+    database : 'taller4'
   });
 
   connection.connect(function(err) {
@@ -100,39 +56,15 @@ module.exports = function(app, mountPoint){
 
   //---------------------Enrutamientos--------------------------
   router.get('/', function(req, res, next) {
-    res.render('index.ejs', { title: 'Droguería Cámbulos'});
+    res.render('index.ejs', { title: 'Colegio'});
   });
 
-  router.get('/empleados', function(req, res, next) {
-    res.render('index.ejs', { title: 'Empleados' });
+  router.get('/alumnos', function(req, res, next) {
+    res.render('index.ejs', { title: 'Alumnos' });
   });
 
-  router.get('/clientes', function(req, res, next) {
-    res.render('index.ejs', { title: 'Clientes' });
-  });
-
-  router.get('/articulos', function(req, res, next) {
-    res.render('index.ejs', { title: 'Artículos' });
-  });
-
-  router.get('/proveedores', function(req, res, next) {
-    res.render('index.ejs', { title: 'Proveedores' });
-  });
-
-  router.get('/medicamentos', function(req, res, next) {
-    res.render('index.ejs', { title: 'Medicamentos' });
-  });
-
-  router.get('/casificacion', function(req, res, next) {
-    res.render('index.ejs', { title: 'clasificación de medicamentos' });
-  });
-
-  router.get('/pedidos', function(req, res, next) {
-    res.render('index.ejs', { title: 'Pedidos' });
-  });
-
-  router.get('/universidades', function(req, res, next) {
-    res.render('index.ejs', { title: 'Universidades' });
+  router.get('/notas', function(req, res, next) {
+    res.render('index.ejs', { title: 'Notas' });
   });
 
   /*------------Sockets-----------------*/
@@ -147,10 +79,10 @@ module.exports = function(app, mountPoint){
       console.log("The client is ready.");
       var path = info.path;
       var data = info.data;
-      if (path === '/universidades') {
-        handleU(socket, info, connection);
-      } else if (path === '/pedidos') {
-        handlePedidos(socket, info, connection);
+      if (path === '/fdgdg') {
+        handleNotas(socket, info, connection);
+      } else if (path === '/alumnos') {
+        handleAlumnos(socket, info, connection);
       }
     });
 
